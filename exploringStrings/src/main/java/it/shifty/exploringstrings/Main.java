81a3f532d45985fdab85f5816ab859715d2c7935
shiftyita <54118429+shiftyita@users.noreply.github.com>
2023-04-10 18:48:07 +0200

    add new module

--- exploringStrings/src/main/java/it/shifty/exploringstrings/Main.java
+++ exploringStrings/src/main/java/it/shifty/exploringstrings/Main.java
@@ -2,7 +2,17 @@ package it.shifty.exploringstrings;
 
 public class Main {
     public static void main(String[] args) {
-        String one = "hello";
-        String two = "hello";
+//        String one = "hello";
+//        String two = "hello";
+//
+//        System.out.println(one.equals(two));
+//        System.out.println(one == two);
+
+        Integer i = 76;
+        String three = i.toString();
+        String four = "76";
+
+        System.out.println(three.equals(four));
+        System.out.println(three == four);
     }
 }
